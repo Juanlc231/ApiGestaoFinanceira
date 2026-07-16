9922515ddf26ae9e1c49c7db8a1b3812468e2215
Juanlc231 <juanlcdacosta.2004@gmail.com>
2026-07-15 21:20:33 -0300

    Commit configuracoes

---
+++ Connection/ConnectionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiGestaoFinanceira.Connection
+{
+    public class ConnectionContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConnectionContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
+using ApiGestaoFinanceira.Connection;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ConnectionContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
