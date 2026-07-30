1aebea4659a380cf936218c344632b43fa089348
Juanlc231 <juanlcdacosta.2004@gmail.com>
2026-07-29 22:18:35 -0300

    Terceira-branch: ajustes no .ignore, criacao de classe de geracao de token e validacoes

---
+++ Service/AuthenticationService.cs
@@ -0,0 +1,23 @@
+﻿using ApiGestaoFinanceira.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiGestaoFinanceira.Service
+{
+    public class AuthenticationService
+    {
+        private readonly PasswordService _passwordService = new PasswordService();
+        private readonly UserService _userService;
+
+        public AuthenticationService(UserService userService) => _userService = userService;
+
+        public async Task<User> Authenticate(string email, string password)
+        {
+            var user = await _userService.GetByEmail(email);
+
+            if (!_passwordService.VerifyPassword(password, user.Password))
+                throw new ArgumentException("Email ou senha inválidos.");
+
+            return user;
+        }
+    }
+}
--- Service/UserService.cs
+++ Service/UserService.cs
@@ -1,6 +1,8 @@
 ﻿using ApiGestaoFinanceira.Connection;
 using ApiGestaoFinanceira.Validate;
 using ApiGestaoFinanceira.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace ApiGestaoFinanceira.Service
 {
@@ -9,57 +11,73 @@ namespace ApiGestaoFinanceira.Service
         private readonly ConnectionContext _context;
         private readonly UserValidate _userValidate = new UserValidate();
         private readonly PasswordValidate _passwordValidate = new PasswordValidate();
+        private readonly PasswordService _passwordService = new PasswordService();
 
         public UserService(ConnectionContext context) => _context = context;
 
-        public List<User> Get() 
+        public async Task<List<User>> Get() 
         { 
-            var users = _context.Users.ToList();
+            var users = await _context.Users.ToListAsync();
 
             return users;
         }
 
-        public User GetById(int id) {
+        public async Task<User> GetById(int id) {
 
             if (id <= 0)
                 throw new ArgumentException("Id de usuário inválido.");
 
-            User user = _context.Users.Find(id) ?? throw new KeyNotFoundException("Usuário não encontrado.");
+            User user = await _context.Users.FindAsync(id) ?? throw new KeyNotFoundException("Usuário não encontrado.");
 
             return user;
         }
 
-        public User Insert(User user)
+        public async Task<User> GetByEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentException("Email é obrigatório.");
+
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email) ?? throw new KeyNotFoundException("Usuário não encontrado.");
+
+            return user;
+        }
+
+        public async Task<User> Insert(User user)
         {
             _userValidate.Validate(user);
+            user.Password = _passwordService.HashPassword(user.Password);
+
             _context.Users.Add(user);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return user;
         }
 
-        public int Update(User updatedUser, int id) 
+        public async Task<int> Update(User updatedUser, int id) 
         {
             _userValidate.Validate(updatedUser);
 
-            var user = GetById(id);
+            var user =  await GetById(id);
 
             user.Name = updatedUser.Name;
             user.Email = updatedUser.Email;
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return user.Id;
         }
 
-        public void ResetPassword(int id, string newPassword, string confirmNewPassword)
+        public async Task ResetPassword(string email, string newPassword, string confirmNewPassword)
         {
-            var user = GetById(id);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+
+            if (user == null)
+                throw new KeyNotFoundException("Usuário não encontrado.");
+
             _passwordValidate.Validate(newPassword, confirmNewPassword);
 
             user.Password = newPassword;
-
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
     }
 }
